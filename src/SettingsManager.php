e820a67aa54777b02edd1d993881a91f8eb669a7
fm-labs <flowmotionlabs@gmail.com>
2017-10-23 12:25:47 +0200

    batch update

--- src/SettingsManager.php
+++ src/SettingsManager.php
@@ -2,24 +2,18 @@
 
 namespace Settings;
 
-use Cake\Event\Event;
-use Cake\Event\EventDispatcherInterface;
-use Cake\Event\EventManager;
+use Cake\Core\Configure;
 use Cake\Form\Schema;
-use Cake\ORM\TableRegistry;
 use Cake\Utility\Hash;
+use Cake\Utility\Inflector;
+use Cake\Utility\Text;
 
 /**
  * Class SettingsManager
  * @package Settings
  */
-class SettingsManager implements EventDispatcherInterface
+class SettingsManager
 {
-    /**
-     * @var string
-     */
-    protected $_scope;
-
     /**
      * @var array
      */
@@ -35,56 +29,22 @@ class SettingsManager implements EventDispatcherInterface
      */
     protected $_compiled = [];
 
-    /**
-     * @param array $settings
-     * @param string $scope
-     */
-    public function __construct($scope = 'default', $settings = [])
+    public function describe()
     {
-        $this->_scope = $scope;
-        $this->_settings = $settings;
+        $schema = $this->buildFormSchema(new Schema());
+        $inputs = $this->buildFormInputs();
 
-        if (!$this->_scope) {
-            throw new \InvalidArgumentException("SettingsManager: Scope can not be empty");
+        $result = [];
+        foreach ($this->_settings as $namespace => $_settings) {
+            foreach (array_keys($_settings) as $key) {
+                $fieldKey = $namespace . '.' . $key;
+                $result[$namespace][$key] = [
+                    'field' => $schema->field($fieldKey),
+                    'input' => $inputs[$fieldKey]
+                ];
+            }
         }
-    }
-
-    /**
-     * Load available setting definitions
-     */
-    protected function _loadSettings()
-    {
-        if (!empty($this->_settings)) {
-            return;
-        }
-
-        // a) read settings.php from each plugin
-
-        // b) collect settings definitions with event
-        $event = $this->dispatchEvent('Settings.get');
-        $this->_settings = (array)$event->result;
-    }
-
-    /**
-     * Load settings values from persistent storage
-     */
-    protected function _loadValues()
-    {
-        if (!empty($this->_values)) {
-            return;
-        }
-
-        $values = [];
-        $settings = TableRegistry::get('Settings.Settings')
-            ->find()
-            ->where(['Settings.scope' => $this->_scope])
-            ->all();
-
-        foreach ($settings as $setting) {
-            $values[$setting->key] = $setting->value;
-        }
-
-        $this->_values = $values;
+        return $result;
     }
 
     /**
@@ -93,7 +53,6 @@ class SettingsManager implements EventDispatcherInterface
      */
     public function buildFormSchema(Schema $schema)
     {
-        $this->_loadSettings();
         foreach ($this->_settings as $namespace => $settings) {
             foreach ($settings as $key => $config) {
                 $columnConfig = array_diff_key($config, ['inputType' => null, 'input' => null, 'default' => null]);
@@ -110,7 +69,6 @@ class SettingsManager implements EventDispatcherInterface
      */
     public function buildFormInputs()
     {
-        $this->_loadSettings();
         $inputs = [];
         /*
         $autoInputType = function($dataType) {
@@ -128,7 +86,7 @@ class SettingsManager implements EventDispatcherInterface
             foreach ($settings as $key => $config) {
 
                 $fieldKey = $namespace . '.' . $key;
-                $config += ['input' => [], 'default' => null];
+                $config += ['input' => [], 'default' => null, 'type' => null];
 
                 $input = $config['input'];
                 unset($config['input']);
@@ -136,65 +94,80 @@ class SettingsManager implements EventDispatcherInterface
                     $input = ['type' => $input];
                 }
 
+                $label = Inflector::humanize(Text::slug($key, ' '));
+                if (isset($config['label'])) {
+                    $label = $config['label'];
+                    unset($config['label']);
+                }
+
                 $defaultInput = [
                     'type' => null,
-                    'label' => $namespace . '.' . $key,
+                    'label' => $label,
                     'default' => $config['default'],
+                    'value' => ($this->value($fieldKey)) ?: Configure::read($fieldKey)
                 ];
                 $input = array_merge($defaultInput, $input);
+
+                if (!$input['type']) {
+                    $input['type'] = $this->_mapTypeToInputType($config['type']);
+                }
+
                 $inputs[$fieldKey] = $input;
             }
         }
         return $inputs;
     }
 
+    protected function _mapTypeToInputType($type)
+    {
+        switch($type) {
+            case "boolean":
+                return "checkbox";
+
+            case "text":
+            case "html":
+                return "textarea";
+
+            case "integer":
+            case "double":
+            case "decimal":
+                return "numeric";
+
+            case "string":
+                return "text";
+
+            default:
+                return $type;
+        }
+
+    }
+
     /**
+     * Get value
+     *
      * @param $key
      * @return null
      */
     public function value($key)
     {
-        $this->_loadValues();
-
         return (isset($this->_values[$key])) ? $this->_values[$key] : null;
     }
 
-    /**
-     * @return array
-     */
-    public function getSettings()
+    public function add($namespace, $key, array $config = [])
     {
-        $this->_loadSettings();
-
-        return $this->_settings;
-    }
-
-    /**
-     * @return array
-     */
-    public function getCompiled()
-    {
-        if (!empty($this->_compiled)) {
-            return $this->_compiled;
-        }
-
-        $this->_loadSettings();
-        $this->_loadValues();
-
-        $compiled = [];
-        foreach ($this->_settings as $namespace => $settings) {
-            foreach ($settings as $setting => $config) {
-                $key = $namespace . '.' . $setting;
-                $value = (isset($this->_values[$key])) ? $this->_values[$key] : null;
-
-                $compiled[$key] = $value;
+        if (is_array($key)) {
+            foreach ($key as $_key => $_config) {
+                $this->add($namespace, $_key, $_config);
             }
+            return $this;
         }
-
-        return $this->_compiled = $compiled;
+        $this->_settings[$namespace][$key] = $config;
+        return $this;
     }
 
     /**
+     * Apply values
+     *
      * @param array $values
      * @return $this
      */
@@ -210,43 +183,34 @@ class SettingsManager implements EventDispatcherInterface
     }
 
     /**
-     * @return int
-     * @TODO Refactor using config engine
+     * @return array
      */
-    public function dump()
+    public function getSettings()
     {
-        $path = SETTINGS . 'settings_' . $this->_scope . '.php';
-        $contents = '<?php' . "\n" . 'return ' . var_export($this->getCompiled(), true) . ';';
-
-        return file_put_contents($path, $contents);
+        return $this->_settings;
     }
 
     /**
-     * Wrapper for creating and dispatching events.
-     *
-     * Returns a dispatched event.
-     *
-     * @param string $name Name of the event.
-     * @param array|null $data Any value you wish to be transported with this event to
-     * it can be read by listeners.
-     * @param object|null $subject The object that this event applies to
-     * ($this by default).
-     *
-     * @return \Cake\Event\Event
+     * @return array
      */
-    public function dispatchEvent($name, $data = null, $subject = null)
+    public function getCompiled()
     {
-        return $this->eventManager()->dispatch(new Event($name, $this, $data));
+        if (!empty($this->_compiled)) {
+            return $this->_compiled;
+        }
+
+        $compiled = [];
+        foreach ($this->_settings as $namespace => $settings) {
+            foreach ($settings as $setting => $config) {
+                $key = $namespace . '.' . $setting;
+                $value = (isset($this->_values[$key])) ? $this->_values[$key] : null;
+
+                $compiled[$key] = $value;
+            }
+        }
+
+        return $this->_compiled = $compiled;
     }
 
-    /**
-     * Returns the global Cake\Event\EventManager manager instance.
-     *
-     * @param \Cake\Event\EventManager|null $eventManager the eventManager to set
-     * @return \Cake\Event\EventManager
-     */
-    public function eventManager(EventManager $eventManager = null)
-    {
-        return EventManager::instance();
-    }
+
 }
